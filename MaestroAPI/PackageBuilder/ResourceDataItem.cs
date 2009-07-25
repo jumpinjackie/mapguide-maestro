31896ed965d9860ed9085ffedd7d6d17e1a3ea9a
ksgeograf <unknown.user@emailaddr>
2009-07-25 10:20:01 +0000

    Maestro: added missing files from last commit
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@4083 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MaestroAPI/PackageBuilder/ResourceDataItem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSGeo.MapGuide.MaestroAPI.PackageBuilder
+{
+    /// <summary>
+    /// Represents a data item on a resource
+    /// </summary>
+    public class ResourceDataItem
+    {
+        private string m_resourceName;
+        private string m_contentType;
+        private string m_filename;
+        private string m_dataType;
+        private EntryTypeEnum m_entryType;
+
+        public ResourceDataItem(string resourceName, string contentType, string filename, string dataType)
+        {
+            m_resourceName = resourceName;
+            m_contentType = contentType;
+            m_filename = filename;
+            m_dataType = dataType;
+            m_entryType = EntryTypeEnum.Regular;
+        }
+
+        public ResourceDataItem(ResourceDataItem rdi)
+        {
+            m_resourceName = rdi.m_resourceName;
+            m_contentType = rdi.m_contentType;
+            m_filename = rdi.m_filename;
+            m_dataType = rdi.m_dataType;
+            m_entryType = rdi.m_entryType;
+        }
+
+        public string ResourceName
+        {
+            get { return m_resourceName; }
+            set { m_resourceName = value; }
+        }
+
+        public string ContentType
+        {
+            get { return m_contentType; }
+            set { m_contentType = value; }
+        }
+
+        public string Filename
+        {
+            get { return m_filename; }
+            set { m_filename = value; }
+        }
+
+        public EntryTypeEnum EntryType
+        {
+            get { return m_entryType; }
+            set { m_entryType = value; }
+        }
+
+        public string DataType
+        {
+            get { return m_dataType; }
+            set { m_dataType = value; }
+        }
+    }
+}
---
+++ MaestroAPI/PackageBuilder/ResourceItem.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSGeo.MapGuide.MaestroAPI.PackageBuilder
+{
+    /// <summary>
+    /// Represents a resource item from a package
+    /// </summary>
+    public class ResourceItem
+    {
+
+        public ResourceItem(string resourcePath, string headerPath, string contentPath)
+        {
+            m_originalResourcePath = m_resourcePath = resourcePath;
+            m_headerpath = headerPath;
+            m_contentpath = contentPath;
+            m_entryType = EntryTypeEnum.Regular;
+            m_items = new List<ResourceDataItem>();
+            m_isFolder = m_originalResourcePath.EndsWith("/");
+        }
+
+        public ResourceItem(ResourceItem ri)
+        {
+            m_originalResourcePath = ri.m_originalResourcePath;
+            m_headerpath = ri.m_headerpath;
+            m_contentpath = ri.m_contentpath;
+            m_resourcePath = ri.m_resourcePath;
+            m_entryType = ri.m_entryType;
+            m_isFolder = ri.m_isFolder;
+            m_items = new List<ResourceDataItem>();
+            foreach (ResourceDataItem rdi in ri.m_items)
+                m_items.Add(new ResourceDataItem(rdi));
+        }
+
+        private string m_originalResourcePath;
+        private string m_headerpath;
+        private string m_contentpath;
+        private string m_resourcePath;
+        private EntryTypeEnum m_entryType;
+        private List<ResourceDataItem> m_items;
+        private bool m_isFolder;
+
+        public bool IsFolder
+        {
+            get { return m_isFolder; }
+            set { m_isFolder = true; }
+        }
+
+        public List<ResourceDataItem> Items
+        {
+            get { return m_items; }
+            set { m_items = value; }
+        }
+
+        public EntryTypeEnum EntryType
+        {
+            get { return m_entryType; }
+            set { m_entryType = value; }
+        }
+
+
+        public string OriginalResourcePath
+        {
+            get { return m_originalResourcePath; }
+            set { m_originalResourcePath = value; }
+        }
+
+        public string ResourcePath
+        {
+            get { return m_resourcePath; }
+            set { m_resourcePath = value; }
+        }
+
+        public string Contentpath
+        {
+            get { return m_contentpath; }
+            set { m_contentpath = value; }
+        }
+
+        public string Headerpath
+        {
+            get { return m_headerpath; }
+            set { m_headerpath = value; }
+        }
+    }
+}
