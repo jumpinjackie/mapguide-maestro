5c0b32173e23be85746b4c8b1b5884d6655088c2
ksgeograf <unknown.user@emailaddr>
2009-08-11 21:21:53 +0000

    Maestro: Undo unwanted font change
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@4153 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Maestro/FormExpression.Designer.cs
+++ Maestro/FormExpression.Designer.cs
@@ -151,7 +151,7 @@ namespace OSGeo.MapGuide.Maestro
             // ExpressionText
             // 
             this.ExpressionText.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ExpressionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExpressionText.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ExpressionText.HideSelection = false;
             this.ExpressionText.Location = new System.Drawing.Point(0, 25);
             this.ExpressionText.Multiline = true;
