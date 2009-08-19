5633c0070c0d075d9eb03dae5aa84c73a61a6724
ksgeograf <unknown.user@emailaddr>
2009-08-19 20:07:12 +0000

    Maestro: Moved the sln file to the root folder, for easy location on fresh checkouts. Updated the assembly version numbers. Rebuild with new extra files.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@4164 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Maestro/ResourceBrowser/Properties/AssemblyInfo.cs
+++ Maestro/ResourceBrowser/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.4099")]
+[assembly: AssemblyFileVersion("1.1.0.4164")]
--- MgCooker/Properties/AssemblyInfo.cs
+++ MgCooker/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.4099")]
-[assembly: AssemblyFileVersion("1.1.0.4099")]
+[assembly: AssemblyVersion("1.1.0.4164")]
+[assembly: AssemblyFileVersion("1.1.0.4164")]
