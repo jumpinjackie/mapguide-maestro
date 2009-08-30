b4b22a961461a1f2d2a6817e6a08d707ea5cc8d9
ksgeograf <unknown.user@emailaddr>
2009-08-30 16:18:06 +0000

    Maestro: Version bump and rebuild
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@4205 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Maestro/MaestroEditorInterface/AssemblyInfo.cs
+++ Maestro/MaestroEditorInterface/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.1.0.4177")]
+[assembly: AssemblyVersion("1.1.0.4203")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
