43ca7d5f0a4aef9f5259b9c9d6365d50a4c7b6af
ksgeograf <unknown.user@emailaddr>
2009-11-25 20:01:54 +0000

    Maestro: Rebuild due to error in previous build.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@4358 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MaestroAPI/PackageBuilder/Properties/AssemblyInfo.cs
+++ MaestroAPI/PackageBuilder/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.0.4357")]
-[assembly: AssemblyFileVersion("1.1.0.4357")]
+[assembly: AssemblyVersion("1.1.0.4358")]
+[assembly: AssemblyFileVersion("1.1.0.4358")]
