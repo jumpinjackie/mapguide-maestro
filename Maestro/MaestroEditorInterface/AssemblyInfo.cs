8677b8eedc99fabf9d04deebe45e450eab6390e7
jumpinjackie <jumpinjackie@gmail.com>
2010-07-07 07:33:32 +0000

    Final preparations for the 2.1 release:
     - #1375: Add croatian translation to installer
     - Update version numbers of all related assemblies
     - Upgrade Installer.sln to VS 2008 format
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@4996 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Maestro/MaestroEditorInterface/AssemblyInfo.cs
+++ Maestro/MaestroEditorInterface/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.1.0.4936")]
+[assembly: AssemblyVersion("2.1.0.4996")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
