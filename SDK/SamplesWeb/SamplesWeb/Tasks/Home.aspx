11f1a82cd1f3a6bba56b9cf1fe0190b5982a8211
jumpinjackie <jumpinjackie@gmail.com>
2011-05-24 18:35:13 +0000

    #1676: Update the web sample with example for listing IDs of selected features
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@5851 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- SDK/SamplesWeb/SamplesWeb/Tasks/Home.aspx
+++ SDK/SamplesWeb/SamplesWeb/Tasks/Home.aspx
@@ -7,30 +7,44 @@
     <title>Home</title>
     <script type="text/javascript">
     
-        function Go(url, extraParams)
+        function Go()
         {
             var map = parent.parent.GetMapFrame();
+            var form = parent.parent.GetFormFrame();
             
-            url += "?SESSION=" + map.GetSessionId();
-            url += "&MAPNAME=" + map.GetMapName();
+            var url = arguments[0];
+            var params = new Array();
+            params.push("SESSION");
+            params.push(map.GetSessionId());
+            params.push("MAPNAME");
+            params.push(map.GetMapName());
+            params.push("SELECTION");
+            params.push(encodeURIComponent(map.GetSelectionXML()));
             
-            if (extraParams)
-                url += "&" + extraParams;
+            if (arguments.length > 1)
+            {
+                for (var i = 1; i < arguments.length; i++)
+                {
+                    params.push(arguments[i]);
+                }
+            }
             
-            window.location = url;
+            form.Submit(url, params, "taskPaneFrame"); //The name of the task pane frame
         }
     
     </script>
 </head>
 <body>
+    <form
     <p>At any time, click the <strong>home button</strong> in the task bar to return to this list of samples.</p>
     
     <p>Samples</p>
     <ul>
-        <li><a href="#" onclick="Go('ToggleParcelsLayer.aspx',null)">Add/Remove Parcels Layer</a></li>
-        <li><a href="#" onclick="Go('AddTracksLayer.aspx',null)">Add Tracks Layer</a></li>
-        <li><a href="#" onclick="Go('ToggleGroupVisibility.aspx','GROUPNAME=Base Map')">Toggle "Base Map" Group</a></li>
-        <li><a href="#" onclick="Go('ToggleLayerVisibility.aspx','LAYERNAME=Parcels')">Toggle "Parcels" Layer</a></li>
+        <li><a href="#" onclick="Go('../SamplesWeb/Tasks/ToggleParcelsLayer.aspx')">Add/Remove Parcels Layer</a></li>
+        <li><a href="#" onclick="Go('../SamplesWeb/Tasks/AddTracksLayer.aspx')">Add Tracks Layer</a></li>
+        <li><a href="#" onclick="Go('../SamplesWeb/Tasks/ListSelection.aspx')">List Selected Features</a></li>
+        <li><a href="#" onclick="Go('../SamplesWeb/Tasks/ToggleGroupVisibility.aspx','GROUPNAME','Base Map')">Toggle "Base Map" Group</a></li>
+        <li><a href="#" onclick="Go('../SamplesWeb/Tasks/ToggleLayerVisibility.aspx','LAYERNAME','Parcels')">Toggle "Parcels" Layer</a></li>
     </ul>
 </body>
 </html>
---
+++ SDK/SamplesWeb/SamplesWeb/Tasks/ListSelection.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using OSGeo.MapGuide.MaestroAPI;
+using OSGeo.MapGuide.MaestroAPI.Services;
+using OSGeo.MapGuide.MaestroAPI.Mapping;
+using System.Text;
+
+namespace SamplesWeb.Tasks
+{
+    public partial class ListSelection : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string agent = ConfigurationManager.AppSettings["MapAgentUrl"];
+
+            IServerConnection conn = ConnectionProviderRegistry.CreateConnection(
+                "Maestro.Http",
+                "Url", agent,
+                "SessionId", Request.Params["SESSION"]);
+
+            IMappingService mpSvc = (IMappingService)conn.GetService((int)ServiceType.Mapping);
+            string rtMapId = "Session:" + conn.SessionID + "//" + Request.Params["MAPNAME"] + ".Map";
+
+            RuntimeMap rtMap = mpSvc.OpenMap(rtMapId);
+
+            string xml = Request.Params["SELECTION"];
+
+            //The map selection contains one or more layer selections
+            //each containing a one or more sets of identity property values
+            //(because a feature may have multiple identity properties)
+
+            MapSelection selection = new MapSelection(rtMap, HttpUtility.UrlDecode(xml));
+            if (selection.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < selection.Count; i++)
+                {
+                    MapSelection.LayerSelection layerSel = selection[i];
+                    sb.Append("<p>Layer: " + layerSel.Layer.Name + "(" + layerSel.Count + ")");
+                    sb.Append("<table>");
+                    
+                    for (int j = 0; j < layerSel.Count; j++)
+                    {
+                        sb.Append("<tr>");
+                        object[] values = layerSel[j];
+                        for (int k = 0; k < values.Length; k++)
+                        {
+                            sb.Append("<td>");
+                            sb.Append(values[k].ToString());
+                            sb.Append("</td>");
+                        }
+                        sb.Append("</tr>");
+                    }
+                    sb.Append("</table>");
+
+                    lblMessage.Text = "Showing IDs of selected features";
+
+                    result.InnerHtml = sb.ToString();
+                }
+            }
+            else
+            {
+                lblMessage.Text = "Nothing selected";
+            }
+        }
+    }
+}
