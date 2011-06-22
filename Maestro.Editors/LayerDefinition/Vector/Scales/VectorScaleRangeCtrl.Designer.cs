e94440d7d0a0874ddc45bcc818dfe85c85ab01c4
jumpinjackie <jumpinjackie@gmail.com>
2011-06-22 17:13:41 +0000

    #1726: Flip on localizable flag
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@5942 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Maestro.Editors/LayerDefinition/Vector/Scales/VectorScaleRangeCtrl.Designer.cs
+++ Maestro.Editors/LayerDefinition/Vector/Scales/VectorScaleRangeCtrl.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VectorScaleRangeCtrl));
             this.chkPoint = new System.Windows.Forms.CheckBox();
             this.chkLine = new System.Windows.Forms.CheckBox();
             this.chkArea = new System.Windows.Forms.CheckBox();
@@ -40,89 +41,59 @@
             // 
             // chkPoint
             // 
-            this.chkPoint.Dock = System.Windows.Forms.DockStyle.Top;
-            this.chkPoint.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.chkPoint, "chkPoint");
             this.chkPoint.Name = "chkPoint";
-            this.chkPoint.Size = new System.Drawing.Size(598, 24);
-            this.chkPoint.TabIndex = 0;
-            this.chkPoint.Text = "Display Points (Note: Symbol previews only show the symbol. It doesn\'t consider c" +
-                "olor overrides or size/rotation settings)";
             this.chkPoint.UseVisualStyleBackColor = true;
             this.chkPoint.CheckedChanged += new System.EventHandler(this.chkPoint_CheckedChanged);
             // 
             // chkLine
             // 
-            this.chkLine.Dock = System.Windows.Forms.DockStyle.Top;
-            this.chkLine.Location = new System.Drawing.Point(0, 121);
+            resources.ApplyResources(this.chkLine, "chkLine");
             this.chkLine.Name = "chkLine";
-            this.chkLine.Size = new System.Drawing.Size(598, 24);
-            this.chkLine.TabIndex = 2;
-            this.chkLine.Text = "Display Lines";
             this.chkLine.UseVisualStyleBackColor = true;
             this.chkLine.CheckedChanged += new System.EventHandler(this.chkLine_CheckedChanged);
             // 
             // chkArea
             // 
-            this.chkArea.Dock = System.Windows.Forms.DockStyle.Top;
-            this.chkArea.Location = new System.Drawing.Point(0, 242);
+            resources.ApplyResources(this.chkArea, "chkArea");
             this.chkArea.Name = "chkArea";
-            this.chkArea.Size = new System.Drawing.Size(598, 24);
-            this.chkArea.TabIndex = 4;
-            this.chkArea.Text = "Display Areas";
             this.chkArea.UseVisualStyleBackColor = true;
             this.chkArea.CheckedChanged += new System.EventHandler(this.chkArea_CheckedChanged);
             // 
             // chkComposite
             // 
-            this.chkComposite.Dock = System.Windows.Forms.DockStyle.Top;
-            this.chkComposite.Location = new System.Drawing.Point(0, 363);
+            resources.ApplyResources(this.chkComposite, "chkComposite");
             this.chkComposite.Name = "chkComposite";
-            this.chkComposite.Size = new System.Drawing.Size(598, 24);
-            this.chkComposite.TabIndex = 6;
-            this.chkComposite.Text = "Display Composite";
             this.chkComposite.UseVisualStyleBackColor = true;
             this.chkComposite.CheckedChanged += new System.EventHandler(this.chkComposite_CheckedChanged);
             // 
             // compList
             // 
-            this.compList.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.compList.Location = new System.Drawing.Point(0, 387);
+            resources.ApplyResources(this.compList, "compList");
             this.compList.Name = "compList";
-            this.compList.Size = new System.Drawing.Size(598, 108);
-            this.compList.TabIndex = 7;
             this.compList.ItemChanged += new System.EventHandler(this.OnItemChanged);
             // 
             // areaList
             // 
-            this.areaList.Dock = System.Windows.Forms.DockStyle.Top;
-            this.areaList.Location = new System.Drawing.Point(0, 266);
+            resources.ApplyResources(this.areaList, "areaList");
             this.areaList.Name = "areaList";
-            this.areaList.Size = new System.Drawing.Size(598, 97);
-            this.areaList.TabIndex = 5;
             this.areaList.ItemChanged += new System.EventHandler(this.OnItemChanged);
             // 
             // lineList
             // 
-            this.lineList.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lineList.Location = new System.Drawing.Point(0, 145);
+            resources.ApplyResources(this.lineList, "lineList");
             this.lineList.Name = "lineList";
-            this.lineList.Size = new System.Drawing.Size(598, 97);
-            this.lineList.TabIndex = 3;
             this.lineList.ItemChanged += new System.EventHandler(this.OnItemChanged);
             // 
             // pointList
             // 
-            this.pointList.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pointList.Location = new System.Drawing.Point(0, 24);
+            resources.ApplyResources(this.pointList, "pointList");
             this.pointList.Name = "pointList";
-            this.pointList.Size = new System.Drawing.Size(598, 97);
-            this.pointList.TabIndex = 1;
             this.pointList.ItemChanged += new System.EventHandler(this.OnItemChanged);
             // 
             // VectorScaleRangeCtrl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.Controls.Add(this.compList);
             this.Controls.Add(this.chkComposite);
             this.Controls.Add(this.areaList);
@@ -132,7 +103,7 @@
             this.Controls.Add(this.pointList);
             this.Controls.Add(this.chkPoint);
             this.Name = "VectorScaleRangeCtrl";
-            this.Size = new System.Drawing.Size(598, 495);
+            resources.ApplyResources(this, "$this");
             this.ResumeLayout(false);
 
         }
