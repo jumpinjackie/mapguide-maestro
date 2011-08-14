eeca9b5a04b2a268efb6c7c1347882b77696765a
jumpinjackie <jumpinjackie@gmail.com>
2011-08-14 13:09:25 +0000

    Updates for Mono and Localization
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@6064 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Maestro.Editors/MapDefinition/ExtentCalculationDialog.Designer.cs
+++ Maestro.Editors/MapDefinition/ExtentCalculationDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExtentCalculationDialog));
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.txtUpperY = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
@@ -54,8 +55,7 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.groupBox2, "groupBox2");
             this.groupBox2.Controls.Add(this.txtUpperY);
             this.groupBox2.Controls.Add(this.label8);
             this.groupBox2.Controls.Add(this.txtUpperX);
@@ -67,160 +67,101 @@
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label9);
             this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.groupBox2.Location = new System.Drawing.Point(12, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(760, 80);
-            this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Final Extents";
             // 
             // txtUpperY
             // 
-            this.txtUpperY.Location = new System.Drawing.Point(280, 50);
+            resources.ApplyResources(this.txtUpperY, "txtUpperY");
             this.txtUpperY.Name = "txtUpperY";
             this.txtUpperY.ReadOnly = true;
-            this.txtUpperY.Size = new System.Drawing.Size(96, 20);
-            this.txtUpperY.TabIndex = 9;
             // 
             // label8
             // 
             this.label8.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label8.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label8.Location = new System.Drawing.Point(256, 50);
+            resources.ApplyResources(this.label8, "label8");
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(16, 16);
-            this.label8.TabIndex = 8;
-            this.label8.Text = "Y";
             // 
             // txtUpperX
             // 
-            this.txtUpperX.Location = new System.Drawing.Point(144, 50);
+            resources.ApplyResources(this.txtUpperX, "txtUpperX");
             this.txtUpperX.Name = "txtUpperX";
             this.txtUpperX.ReadOnly = true;
-            this.txtUpperX.Size = new System.Drawing.Size(96, 20);
-            this.txtUpperX.TabIndex = 7;
             // 
             // label7
             // 
             this.label7.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label7.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label7.Location = new System.Drawing.Point(120, 50);
+            resources.ApplyResources(this.label7, "label7");
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(16, 16);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "X";
             // 
             // txtLowerY
             // 
-            this.txtLowerY.Location = new System.Drawing.Point(280, 24);
+            resources.ApplyResources(this.txtLowerY, "txtLowerY");
             this.txtLowerY.Name = "txtLowerY";
             this.txtLowerY.ReadOnly = true;
-            this.txtLowerY.Size = new System.Drawing.Size(96, 20);
-            this.txtLowerY.TabIndex = 5;
             // 
             // label6
             // 
             this.label6.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label6.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label6.Location = new System.Drawing.Point(256, 24);
+            resources.ApplyResources(this.label6, "label6");
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(16, 16);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "Y";
             // 
             // txtLowerX
             // 
-            this.txtLowerX.Location = new System.Drawing.Point(144, 24);
+            resources.ApplyResources(this.txtLowerX, "txtLowerX");
             this.txtLowerX.Name = "txtLowerX";
             this.txtLowerX.ReadOnly = true;
-            this.txtLowerX.Size = new System.Drawing.Size(96, 20);
-            this.txtLowerX.TabIndex = 3;
             // 
             // label5
             // 
             this.label5.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label5.Location = new System.Drawing.Point(120, 24);
+            resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(16, 16);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "X";
             // 
             // label4
             // 
             this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label4.Location = new System.Drawing.Point(16, 50);
+            resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 16);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Upper right";
             // 
             // label9
             // 
             this.label9.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.label9.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.label9.Location = new System.Drawing.Point(16, 24);
+            resources.ApplyResources(this.label9, "label9");
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(88, 16);
-            this.label9.TabIndex = 0;
-            this.label9.Text = "Lower left";
             // 
             // grdCalculations
             // 
             this.grdCalculations.AllowUserToAddRows = false;
             this.grdCalculations.AllowUserToDeleteRows = false;
-            this.grdCalculations.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.grdCalculations, "grdCalculations");
             this.grdCalculations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grdCalculations.Location = new System.Drawing.Point(12, 172);
             this.grdCalculations.Name = "grdCalculations";
             this.grdCalculations.ReadOnly = true;
-            this.grdCalculations.Size = new System.Drawing.Size(760, 116);
-            this.grdCalculations.TabIndex = 6;
             // 
             // btnAccept
             // 
-            this.btnAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAccept.Enabled = false;
-            this.btnAccept.Location = new System.Drawing.Point(616, 327);
+            resources.ApplyResources(this.btnAccept, "btnAccept");
             this.btnAccept.Name = "btnAccept";
-            this.btnAccept.Size = new System.Drawing.Size(75, 23);
-            this.btnAccept.TabIndex = 7;
-            this.btnAccept.Text = "Accept";
             this.btnAccept.UseVisualStyleBackColor = true;
             this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
             // 
             // btnClose
             // 
-            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClose.Enabled = false;
-            this.btnClose.Location = new System.Drawing.Point(697, 327);
+            resources.ApplyResources(this.btnClose, "btnClose");
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
-            this.btnClose.TabIndex = 8;
-            this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 145);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 13);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Calculation Results";
             // 
             // prgCalculations
             // 
-            this.prgCalculations.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.prgCalculations.Location = new System.Drawing.Point(12, 294);
+            resources.ApplyResources(this.prgCalculations, "prgCalculations");
             this.prgCalculations.Name = "prgCalculations";
-            this.prgCalculations.Size = new System.Drawing.Size(760, 23);
-            this.prgCalculations.TabIndex = 10;
             // 
             // bgCalculation
             // 
@@ -231,37 +172,24 @@
             // 
             // lblMessage
             // 
-            this.lblMessage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.lblMessage.AutoSize = true;
-            this.lblMessage.Location = new System.Drawing.Point(12, 332);
+            resources.ApplyResources(this.lblMessage, "lblMessage");
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(0, 13);
-            this.lblMessage.TabIndex = 11;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 106);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 13);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Coordinate System";
             // 
             // txtCoordinateSystem
             // 
-            this.txtCoordinateSystem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtCoordinateSystem.Location = new System.Drawing.Point(156, 103);
+            resources.ApplyResources(this.txtCoordinateSystem, "txtCoordinateSystem");
             this.txtCoordinateSystem.Name = "txtCoordinateSystem";
             this.txtCoordinateSystem.ReadOnly = true;
-            this.txtCoordinateSystem.Size = new System.Drawing.Size(616, 20);
-            this.txtCoordinateSystem.TabIndex = 13;
             // 
             // ExtentCalculationDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 362);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            resources.ApplyResources(this, "$this");
             this.ControlBox = false;
             this.Controls.Add(this.txtCoordinateSystem);
             this.Controls.Add(this.label2);
@@ -273,8 +201,6 @@
             this.Controls.Add(this.grdCalculations);
             this.Controls.Add(this.groupBox2);
             this.Name = "ExtentCalculationDialog";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Calculating Extents";
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grdCalculations)).EndInit();
