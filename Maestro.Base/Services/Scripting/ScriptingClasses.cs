f64c05962af4426d4fb45aa72c44b3e42795832b
jumpinjackie <jumpinjackie@gmail.com>
2012-01-30 17:39:13 +0000

    #1934: Add IronPython 2.7.1 to thirdparty. Add a new ScriptingService which provides support for execution and evaluation of python code. Also expose a simple Application object in the global python scripting scope that can be accessed by python code.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@6467 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ Maestro.Base/Services/Scripting/ScriptingClasses.cs
@@ -0,0 +1,91 @@
+﻿#region Disclaimer / License
+// Copyright (C) 2011, Jackie Ng
+// http://trac.osgeo.org/mapguide/wiki/maestro, jumpinjackie@gmail.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Maestro.Shared.UI;
+using ICSharpCode.Core;
+using OSGeo.MapGuide.MaestroAPI;
+
+namespace Maestro.Base.Services.Scripting
+{
+    /// <summary>
+    /// A simplified helper class that is exposed to python scripts
+    /// </summary>
+    public class App
+    {
+        /// <summary>
+        /// The main application window
+        /// </summary>
+        public Workbench MainWindow
+        {
+            get { return Workbench.Instance; }
+        }
+
+        /// <summary>
+        /// The connection manager
+        /// </summary>
+        public ServerConnectionManager ConnectionManager
+        {
+            get { return ServiceRegistry.GetService<ServerConnectionManager>(); }
+        }
+
+        /// <summary>
+        /// Launches the specified url
+        /// </summary>
+        /// <param name="url"></param>
+        public void OpenUrl(string url)
+        {
+            var svc = ServiceRegistry.GetService<UrlLauncherService>();
+            svc.OpenUrl(url);
+        }
+
+        /// <summary>
+        /// Prompts for a question that requires a boolean response
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="question"></param>
+        /// <returns></returns>
+        public bool AskQuestion(string title, string question)
+        {
+            return MessageService.AskQuestion(question, title);
+        }
+
+        /// <summary>
+        /// Displays a message
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="message"></param>
+        public void ShowMessage(string title, string message)
+        {
+            MessageService.ShowMessage(message, title);
+        }
+
+        /// <summary>
+        /// Displays an exception in a dialog
+        /// </summary>
+        /// <param name="ex"></param>
+        public void ShowError(Exception ex)
+        {
+            ErrorDialog.Show(ex);
+        }
+    }
+}
