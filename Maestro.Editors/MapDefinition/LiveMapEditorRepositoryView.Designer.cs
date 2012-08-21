49b880f4a4d6a6e1e7d66673305025a7c0a16a34
jumpinjackie <jumpinjackie@gmail.com>
2012-08-21 10:20:45 +0000

    Missed some files from previous submission
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/Tools/Maestro@6943 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ Maestro.Editors/MapDefinition/LiveMapEditorRepositoryView.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Maestro.Editors.MapDefinition
+{
+    partial class LiveMapEditorRepositoryView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LiveMapEditorRepositoryView));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAddToMap = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnEdit = new System.Windows.Forms.ToolStripButton();
+            this.btnRefresh = new System.Windows.Forms.ToolStripButton();
+            this.repoView = new Maestro.Editors.Common.RepositoryView();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAddToMap,
+            this.toolStripSeparator1,
+            this.btnEdit,
+            this.btnRefresh});
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
+            this.toolStrip1.Name = "toolStrip1";
+            // 
+            // btnAddToMap
+            // 
+            this.btnAddToMap.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.btnAddToMap, "btnAddToMap");
+            this.btnAddToMap.Image = global::Maestro.Editors.Properties.Resources.map__plus;
+            this.btnAddToMap.Name = "btnAddToMap";
+            this.btnAddToMap.Click += new System.EventHandler(this.btnAddToMap_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            resources.ApplyResources(this.btnEdit, "btnEdit");
+            this.btnEdit.Image = global::Maestro.Editors.Properties.Resources.document__pencil;
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnRefresh.Image = global::Maestro.Editors.Properties.Resources.arrow_circle_135;
+            resources.ApplyResources(this.btnRefresh, "btnRefresh");
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // repoView
+            // 
+            resources.ApplyResources(this.repoView, "repoView");
+            this.repoView.Name = "repoView";
+            this.repoView.ItemSelected += new System.EventHandler(this.repoView_ItemSelected);
+            // 
+            // LiveMapEditorRepositoryView
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.repoView);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "LiveMapEditorRepositoryView";
+            resources.ApplyResources(this, "$this");
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private Common.RepositoryView repoView;
+        private System.Windows.Forms.ToolStripButton btnEdit;
+        private System.Windows.Forms.ToolStripButton btnRefresh;
+        private System.Windows.Forms.ToolStripButton btnAddToMap;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
---
+++ Maestro.Editors/MapDefinition/LiveMapEditorRepositoryView.cs
@@ -0,0 +1,100 @@
+﻿#region Disclaimer / License
+// Copyright (C) 2012, Jackie Ng
+// http://trac.osgeo.org/mapguide/wiki/maestro, jumpinjackie@gmail.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OSGeo.MapGuide.MaestroAPI.Services;
+using OSGeo.MapGuide.MaestroAPI;
+using OSGeo.MapGuide.MaestroAPI.Resource;
+using OSGeo.MapGuide.ObjectModels.Common;
+
+namespace Maestro.Editors.MapDefinition
+{
+    public partial class LiveMapEditorRepositoryView : UserControl
+    {
+        public LiveMapEditorRepositoryView()
+        {
+            InitializeComponent();
+        }
+
+        public void Init(IResourceService resSvc, ResourceTypes[] filteredTypes)
+        {
+            repoView.Init(resSvc, false);
+            repoView.ClearResourceTypeFilters();
+            if (filteredTypes != null)
+            {
+                foreach (var rt in filteredTypes)
+                {
+                    repoView.AddResourceTypeFilter(rt);
+                }
+            }
+        }
+
+        private void btnAddToMap_Click(object sender, EventArgs e)
+        {
+            var h = this.RequestAddToMap;
+            if (h != null)
+                h(this, EventArgs.Empty);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            var h = this.RequestEdit;
+            if (h != null)
+                h(this, EventArgs.Empty);
+        }
+
+        public event EventHandler RequestAddToMap;
+
+        public event EventHandler RequestEdit;
+
+        public IRepositoryItem SelectedItem
+        {
+            get { return repoView.SelectedItem; }
+        }
+
+        private void repoView_ItemSelected(object sender, EventArgs e)
+        {
+            var item = repoView.SelectedItem;
+            var condition = (item != null && !item.IsFolder && (item.ResourceType == ResourceTypes.LayerDefinition));
+            btnAddToMap.Enabled = btnEdit.Enabled = condition;
+            btnRefresh.Enabled = !condition;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            var item = repoView.SelectedItem;
+            if (item.IsFolder)
+            {
+                repoView.RefreshModel(item.ResourceId);
+            }
+            else 
+            {
+                var parent = ResourceIdentifier.GetParentFolder(item.ResourceId);
+                repoView.RefreshModel(parent);
+            }
+        }
+    }
+}
