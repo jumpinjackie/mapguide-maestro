f155a5f80b5ccc670c1437653a113026d7e6a71b
Jackie Ng <jumpinjackie@gmail.com>
2017-08-16 03:47:22 +1000

    Set DebugType to full to allow OpenCover to work again. Also update nunit-console path.

--- test_with_coverage.bat
+++ test_with_coverage.bat
@@ -1,6 +1,6 @@
 @echo off
 SET CONFIG=%1
 IF NOT EXIST Reports MKDIR Reports
-Maestro\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -filter:"+[OSGeo.*]* -[*.tests]* -[*.Tests]* -[Irony]*" -register:user -target:"Maestro\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe" -targetargs:"OSGeo.MapGuide.MaestroAPI.Tests\bin\%CONFIG%\OSGeo.MapGuide.MaestroAPI.Tests.dll OSGeo.MapGuide.ObjectModel.Tests\bin\%CONFIG%\OSGeo.MapGuide.ObjectModels.Tests.dll" -output:Reports\OpenCoverCoverage.xml
+Maestro\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -filter:"+[OSGeo.*]* -[*.tests]* -[*.Tests]* -[Irony]*" -register:user -target:"Maestro\packages\NUnit.ConsoleRunner.3.7.0\tools\nunit3-console.exe" -targetargs:"OSGeo.MapGuide.MaestroAPI.Tests\bin\%CONFIG%\OSGeo.MapGuide.MaestroAPI.Tests.dll OSGeo.MapGuide.ObjectModel.Tests\bin\%CONFIG%\OSGeo.MapGuide.ObjectModels.Tests.dll" -output:Reports\OpenCoverCoverage.xml
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-Maestro\packages\ReportGenerator.2.5.2\tools\ReportGenerator.exe -reports:Reports\OpenCoverCoverage.xml -targetdir:Reports
+Maestro\packages\ReportGenerator.2.5.10\tools\ReportGenerator.exe -reports:Reports\OpenCoverCoverage.xml -targetdir:Reports
