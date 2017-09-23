470389b471019a3301f9bbaf24237761d4a77e0b
Jackie Ng <jumpinjackie@gmail.com>
2017-09-24 02:43:09 +1000

    Since we're now multi-targeting it means that OpenCover should work again if we hit it with xunit console runner against the full framework test assemblies. So restore this functionality.

--- test_with_coverage.bat
+++ test_with_coverage.bat
@@ -1,6 +1,7 @@
 @echo off
 SET CONFIG=%1
+SET TARGETFX=net461
 IF NOT EXIST Reports MKDIR Reports
-Packages\opencover\4.6.519\tools\OpenCover.Console.exe -oldstyle -filter:"+[OSGeo.*]* -[*.tests]* -[*.Tests]* -[Irony]*" -register:user -target:"unittest.bat" -output:Reports\OpenCoverCoverage.xml
+Packages\opencover\4.6.519\tools\OpenCover.Console.exe -filter:"+[OSGeo.*]* -[*.tests]* -[*.Tests]* -[Irony]*" -register:user -target:"Packages\xunit.runner.console\2.2.0\tools\xunit.console.exe" -targetargs:"OSGeo.MapGuide.MaestroAPI.Tests\bin\%CONFIG%\%TARGETFX%\OSGeo.MapGuide.MaestroAPI.Tests.dll OSGeo.MapGuide.ObjectModels.Tests\bin\%CONFIG%\%TARGETFX%\OSGeo.MapGuide.ObjectModels.Tests.dll -noshadow" -output:Reports\OpenCoverCoverage.xml -coverbytest:*.Tests.dll
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 Packages\reportgenerator\3.0.0\tools\ReportGenerator.exe -reports:Reports\OpenCoverCoverage.xml -targetdir:Reports
