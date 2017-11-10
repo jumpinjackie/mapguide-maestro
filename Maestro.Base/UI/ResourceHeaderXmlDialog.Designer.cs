f09cf9791cf720dbbf516d0d5eeb0292ff08d60a
Jackie Ng <jumpinjackie@gmail.com>
2017-11-11 01:21:35 +1100

    We have a rich XML editor component (ICSharpCode.TextEditor), so use it for editing raw resource header XML

--- Maestro.Base/UI/ResourceHeaderXmlDialog.Designer.cs
+++ Maestro.Base/UI/ResourceHeaderXmlDialog.Designer.cs
@@ -31,8 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResourceHeaderXmlDialog));
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.txtXml = new System.Windows.Forms.RichTextBox();
             this.lblResourceId = new System.Windows.Forms.Label();
+            this.txtXml = new Maestro.Editors.Generic.XmlTextEditorControl();
             this.SuspendLayout();
             // 
             // btnSave
@@ -49,22 +49,23 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // txtXml
-            // 
-            resources.ApplyResources(this.txtXml, "txtXml");
-            this.txtXml.Name = "txtXml";
-            // 
             // lblResourceId
             // 
             resources.ApplyResources(this.lblResourceId, "lblResourceId");
             this.lblResourceId.Name = "lblResourceId";
             // 
+            // txtXml
+            // 
+            resources.ApplyResources(this.txtXml, "txtXml");
+            this.txtXml.IsReadOnly = false;
+            this.txtXml.Name = "txtXml";
+            // 
             // ResourceHeaderXmlDialog
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             resources.ApplyResources(this, "$this");
-            this.Controls.Add(this.lblResourceId);
             this.Controls.Add(this.txtXml);
+            this.Controls.Add(this.lblResourceId);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Name = "ResourceHeaderXmlDialog";
@@ -77,7 +78,7 @@
 
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.RichTextBox txtXml;
         private System.Windows.Forms.Label lblResourceId;
+        private Editors.Generic.XmlTextEditorControl txtXml;
     }
 }
