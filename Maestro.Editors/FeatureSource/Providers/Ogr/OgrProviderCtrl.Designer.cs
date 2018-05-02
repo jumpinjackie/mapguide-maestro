bbae59b4b62686474240dfbae0ce5fc19e4e47c4
Jackie Ng <jumpinjackie@gmail.com>
2018-05-03 01:16:46 +1000

    #64: Initial implementation of specialized editor for OGR feature sources

---
+++ Maestro.Editors/FeatureSource/Providers/Ogr/OgrProviderCtrl.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Maestro.Editors.FeatureSource.Providers.Ogr
+{
+    partial class OgrProviderCtrl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDataSource = new System.Windows.Forms.TextBox();
+            this.chkReadOnly = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grdOtherProperties = new System.Windows.Forms.DataGridView();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtConnectionStatus = new System.Windows.Forms.TextBox();
+            this.COL_NAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contentPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdOtherProperties)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contentPanel
+            // 
+            this.contentPanel.Controls.Add(this.groupBox3);
+            this.contentPanel.Controls.Add(this.btnTest);
+            this.contentPanel.Controls.Add(this.grdOtherProperties);
+            this.contentPanel.Controls.Add(this.label2);
+            this.contentPanel.Controls.Add(this.chkReadOnly);
+            this.contentPanel.Controls.Add(this.txtDataSource);
+            this.contentPanel.Controls.Add(this.label1);
+            this.contentPanel.Size = new System.Drawing.Size(510, 302);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Source";
+            // 
+            // txtDataSource
+            // 
+            this.txtDataSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDataSource.Location = new System.Drawing.Point(137, 15);
+            this.txtDataSource.Name = "txtDataSource";
+            this.txtDataSource.Size = new System.Drawing.Size(367, 20);
+            this.txtDataSource.TabIndex = 1;
+            this.txtDataSource.TextChanged += new System.EventHandler(this.txtDataSource_TextChanged);
+            // 
+            // chkReadOnly
+            // 
+            this.chkReadOnly.AutoSize = true;
+            this.chkReadOnly.Location = new System.Drawing.Point(137, 41);
+            this.chkReadOnly.Name = "chkReadOnly";
+            this.chkReadOnly.Size = new System.Drawing.Size(76, 17);
+            this.chkReadOnly.TabIndex = 2;
+            this.chkReadOnly.Text = "Read Only";
+            this.chkReadOnly.UseVisualStyleBackColor = true;
+            this.chkReadOnly.CheckedChanged += new System.EventHandler(this.chkReadOnly_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Other Properties";
+            // 
+            // grdOtherProperties
+            // 
+            this.grdOtherProperties.AllowUserToAddRows = false;
+            this.grdOtherProperties.AllowUserToDeleteRows = false;
+            this.grdOtherProperties.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdOtherProperties.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdOtherProperties.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.COL_NAME,
+            this.Value});
+            this.grdOtherProperties.Location = new System.Drawing.Point(137, 64);
+            this.grdOtherProperties.Name = "grdOtherProperties";
+            this.grdOtherProperties.Size = new System.Drawing.Size(367, 146);
+            this.grdOtherProperties.TabIndex = 4;
+            this.grdOtherProperties.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.grdOtherProperties_CellPainting);
+            this.grdOtherProperties.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdOtherProperties_CellValueChanged);
+            this.grdOtherProperties.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.grdOtherProperties_EditingControlShowing);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(17, 228);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(105, 23);
+            this.btnTest.TabIndex = 5;
+            this.btnTest.Text = "Test Connection";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.txtConnectionStatus);
+            this.groupBox3.Location = new System.Drawing.Point(137, 228);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(370, 61);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Messages";
+            // 
+            // txtConnectionStatus
+            // 
+            this.txtConnectionStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConnectionStatus.Location = new System.Drawing.Point(3, 16);
+            this.txtConnectionStatus.Multiline = true;
+            this.txtConnectionStatus.Name = "txtConnectionStatus";
+            this.txtConnectionStatus.ReadOnly = true;
+            this.txtConnectionStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConnectionStatus.Size = new System.Drawing.Size(364, 42);
+            this.txtConnectionStatus.TabIndex = 7;
+            // 
+            // COL_NAME
+            // 
+            this.COL_NAME.HeaderText = "Name";
+            this.COL_NAME.Name = "COL_NAME";
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            // 
+            // OgrProviderCtrl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.HeaderText = "OGR Feature Source";
+            this.Name = "OgrProviderCtrl";
+            this.Size = new System.Drawing.Size(510, 329);
+            this.contentPanel.ResumeLayout(false);
+            this.contentPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdOtherProperties)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDataSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkReadOnly;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView grdOtherProperties;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtConnectionStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn COL_NAME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
---
+++ Maestro.Editors/FeatureSource/Providers/Ogr/OgrProviderCtrl.cs
@@ -0,0 +1,176 @@
+﻿#region Disclaimer / License
+
+// Copyright (C) 2018, Jackie Ng
+// https://github.com/jumpinjackie/mapguide-maestro
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+#endregion Disclaimer / License
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Maestro.Editors.Common;
+using OSGeo.MapGuide.ObjectModels.FeatureSource;
+
+namespace Maestro.Editors.FeatureSource.Providers.Ogr
+{
+    [ToolboxItem(false)]
+    public partial class OgrProviderCtrl : EditorBindableCollapsiblePanel
+    {
+        public OgrProviderCtrl()
+        {
+            InitializeComponent();
+        }
+
+        const string P_DATASOURCE = "DataSource";
+        const string P_READONLY = "ReadOnly";
+
+        private IEditorService _service;
+        private IFeatureSource _fs;
+        private bool _init = false;
+
+        public override void Bind(IEditorService service)
+        {
+            _init = true;
+            _service = service;
+            _fs = (IFeatureSource)_service.GetEditedResource();
+
+            var values = _fs.GetConnectionProperties();
+
+            txtDataSource.Text = values[P_DATASOURCE];
+            chkReadOnly.Checked = values[P_READONLY] == "TRUE";
+
+            var prov = _service.CurrentConnection.FeatureService.GetFeatureProvider("OSGeo.OGR");
+            foreach (var p in prov.ConnectionProperties.Where(p => p.Name != P_DATASOURCE && p.Name != P_READONLY))
+            {
+                var row = new DataGridViewRow();
+                var nameCell = new DataGridViewTextBoxCell();
+                nameCell.Value = p.Name;
+                nameCell.ToolTipText = p.LocalizedName;
+
+                var currentValue = _fs.GetConnectionProperty(p.Name);
+                DataGridViewCell valueCell = null;
+                if (p.Enumerable)
+                {
+                    valueCell = new DataGridViewTextBoxCell();
+                    valueCell.Tag = p;
+                    valueCell.Value = currentValue;
+                }
+                else
+                {
+                    valueCell = new DataGridViewTextBoxCell();
+                    valueCell.Tag = p;
+                    valueCell.Value = currentValue;
+                }
+
+                if (string.IsNullOrEmpty(currentValue) && !string.IsNullOrEmpty(p.DefaultValue))
+                {
+                    valueCell.Value = p.DefaultValue;
+                    _fs.SetConnectionProperty(p.Name, p.DefaultValue);
+                }
+
+                row.Cells.Add(nameCell);
+                row.Cells.Add(valueCell);
+
+                if (p.Protected)
+                    pwdCells.Add(valueCell);
+
+                grdOtherProperties.Rows.Add(row);
+            }
+
+            _init = false;
+        }
+
+        private List<DataGridViewCell> pwdCells = new List<DataGridViewCell>();
+
+        private bool IsPasswordCell(DataGridViewCell cell)
+        {
+            return pwdCells.Contains(cell);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            txtConnectionStatus.Text = string.Empty;
+            //Flush back to session before testing
+            _service.SyncSessionCopy();
+            string result = _service.CurrentConnection.FeatureService.TestConnection(_fs.ResourceID);
+
+            txtConnectionStatus.Text = string.Format(Strings.FdoConnectionStatus, result);
+        }
+
+        private void chkReadOnly_CheckedChanged(object sender, EventArgs e)
+        {
+            if (_init)
+                return;
+
+            _fs.SetConnectionProperty(P_READONLY, chkReadOnly.Checked ? "TRUE" : "FALSE");
+        }
+
+        private void txtDataSource_TextChanged(object sender, EventArgs e)
+        {
+            if (_init)
+                return;
+
+            _fs.SetConnectionProperty(P_DATASOURCE, txtDataSource.Text);
+        }
+
+        private void grdOtherProperties_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewCell cell = grdOtherProperties.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                if (cell != null && IsPasswordCell(cell) && cell.Value != null)
+                {
+                    e.Paint(e.CellBounds, DataGridViewPaintParts.All & ~DataGridViewPaintParts.ContentForeground);
+                    Graphics g = e.Graphics;
+                    g.DrawString(new string('*', cell.Value.ToString().Length), this.Font, new SolidBrush(Color.Black), e.CellBounds); //NOXLATE
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void grdOtherProperties_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            DataGridViewCell cell = grdOtherProperties.SelectedCells[0];
+            if (cell != null)
+            {
+                TextBox t = e.Control as TextBox;
+                if (t != null)
+                {
+                    t.UseSystemPasswordChar = IsPasswordCell(cell);
+                }
+            }
+        }
+
+        private void grdOtherProperties_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //Only care about changes in "Value" cells
+            if (e.ColumnIndex == 1 && e.RowIndex >= 0)
+            {
+                var name = grdOtherProperties[0, e.RowIndex].Value.ToString();
+                var value = grdOtherProperties[e.ColumnIndex, e.RowIndex].Value;
+                _fs.SetConnectionProperty(name, value == null ? string.Empty : value.ToString());
+            }
+        }
+    }
+}
